72fbae70bcb44a4d144bbf8aa1e56b13dc73a108
Nathan Mentley <nathan@nathans-air.lan>
2021-08-16 09:06:21 -0400

    added weather collection codec.

--- Package.swift
+++ Package.swift
@@ -76,9 +76,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 
-        //.package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
-        .package(url: "https://github.com/NozeIO/MicroExpress.git", 
-             from: "0.5.3")
+        .package(url: "https://github.com/NozeIO/Noze.io.git", from: "0.6.7")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -142,7 +140,7 @@ let package = Package(
         .target(
             name: "ServerNio",
             dependencies: [
-                "MicroExpress",
+                "NozeIO",
 
                 "Server"
             ]
