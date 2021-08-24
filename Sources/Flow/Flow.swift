2561df13b430ed94112d23d5534277ad9b25ee96
Nathan Mentley <nathan@nathans-air.lan>
2021-08-23 20:59:39 -0400

    upper case flow

---
+++ Sources/Flow/Flow.swift
@@ -0,0 +1,62 @@
+/// A class that handles the processing of writed up stages. We refer to these stages wired up in a meaningful way as a flow.
+public class Flow {
+    /// The max number of stage executation that can be run in a flow before we'll assume we're in an infinte loop.
+    static let MAX_ITERATIONS: Int = 5_000;
+
+    /// A StageResult indicating that nothing ran.
+    static let NOT_RUN_RESULT: StageResult =
+        StageResult(as: "NOT_RUN");
+
+    /// A StageResult indicating that the flow was interrupted because we believe it was stuck in an infinite loop.
+    static let TOO_MANY_ITERATIONS_RESULT: StageResult =
+        StageResult(as: "TOO_MANY_ITERATIONS");
+
+    /// A dictionary of usable stages for the flow. Keyed by the stage's unique name.
+    private let _stages: [String:BaseStage];
+
+    /// A string of the unique stage name that will be executed first in the flow
+    private let _initialStage: String;
+
+    /**
+    Constructor
+
+     - Parameters:
+        - hasStages: A dictionary of stages keyed by the stage's unique name. These stages will become usable in the flow.
+        - withInitialStage: A string of the unique stage name that will be executed first in the flow.
+     */
+    internal init(hasStages stages: [String:BaseStage], withInitialStage initialStage: String) {
+        _stages = stages;
+        _initialStage = initialStage;
+    }
+
+    /**
+    Runs the flow for the passed in data contained in a FlowContext.
+
+     - Parameters:
+        - for: A flow context that contains the state data used when executing the flow.
+
+    - Returns: A FlowResult containing details related to the flow execution.
+     */
+    public func Execute(for context: FlowContext) throws -> FlowResult {
+        var counter: Int = 0;
+        var nextStage: String? = _initialStage;
+        var result: StageResult = Flow.NOT_RUN_RESULT;
+
+        while nextStage != nil {
+            if counter > Flow.MAX_ITERATIONS {
+                result = Flow.TOO_MANY_ITERATIONS_RESULT;
+                break;
+            }
+
+            if let stage = _stages[nextStage!] {
+                result = try stage.Execute(for: context);
+
+                nextStage = stage.GetNextStage(for: result);
+            }
+
+            counter += 1;
+        }
+
+        return FlowResult(as: result);
+    }
+}
