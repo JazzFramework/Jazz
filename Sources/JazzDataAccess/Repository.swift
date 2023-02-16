7a12cd532f2e47fd798038383681e5ba34aabaa5
Nathan Mentley <nathan@Nathans-MacBook-Air.local>
2023-02-16 14:52:02 -0500

    data access repo should have a default definition for get by id.

--- Sources/JazzDataAccess/Repository.swift
+++ Sources/JazzDataAccess/Repository.swift
@@ -18,11 +18,19 @@ open class Repository<TResource: Storable> {
         return model;
     }
 
-    open func get(id: String, with hints: [QueryHint]) async throws -> TResource {
-        throw DataAccessErrors.notFound(reason: "Could not find resource for \(id).");
-    }
-
     open func get(for criteria: [QueryCriterion], with hints: [QueryHint]) async throws -> [TResource] {
         return [];
+    }
+
+    public final func get(id: String, with hints: [QueryHint]) async throws -> TResource {
+        let results: [TResource] = try await get(for: [IdQueryCriterion(id)], with: []);
+
+        if results.isEmpty {
+            throw DataAccessErrors.notFound(reason: "Could not find resource for \(id).");
+        } else if results.count > 1 {
+            throw DataAccessErrors.notFound(reason: "Could not find resource for \(id), because multiple were found.");
+        }
+
+        return results[0];
     }
 }
---
+++ Tests/DataAccessTests/RepositoryTests.swift
@@ -0,0 +1,10 @@
+import XCTest;
+
+final class RepositoryTests: XCTestCase {
+    func testExample() async throws {
+        //Arrange
+        //Act
+        //Assert
+        XCTAssertEqual("result", "result")
+    }
+}
